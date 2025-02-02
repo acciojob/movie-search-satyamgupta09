5fac01244b6f4c1ed6e6722b0d0c84ac0d2301ff
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-02-03 03:39:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import React from "react";
+import Search from "./Search";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Search Movie</p>
+      <Search />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
